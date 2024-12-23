375ec0608f5251593e0419254aa2b3690d348ccc
AndrewMazurGH <andriyko.mz@gmail.com>
2024-12-23 18:58:53 +0200

    Initial commit

---
+++ apps/office-booking-api/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'office-booking-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/office-booking-api',
+};
